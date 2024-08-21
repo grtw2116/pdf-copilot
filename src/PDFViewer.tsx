ea2658966408873dd762128188b653e74eaa1452
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2024-08-21 16:51:29 +0900

    feat: Add react-pdf for PDF rendering and update dependencies

--- src/PDFViewer.tsx
+++ src/PDFViewer.tsx
@@ -1,31 +1,17 @@
-import { getDocument } from "pdfjs-dist";
-import { useEffect, useRef } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/cjs/Page/AnnotationLayer.css";
+import "react-pdf/dist/cjs/Page/TextLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url,
+).toString();
 
 export default function PDFViewer() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    const loadPDF = async () => {
-      const pdf = await getDocument("../assets/example.pdf").promise;
-      const page = await pdf.getPage(1);
-      const viewport = page.getViewport({ scale: 1.5 });
-
-      const canvas = canvasRef.current;
-      const context = canvas?.getContext("2d");
-      if (canvas && context) {
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
-
-        const renderContext = {
-          canvasContext: context,
-          viewport: viewport,
-        };
-        page.render(renderContext);
-      }
-    };
-
-    loadPDF();
-  }, []);
-
-  return <canvas ref={canvasRef} />;
+  return (
+    <Document file="../assets/example.pdf">
+      <Page pageNumber={1} />
+      <Page pageNumber={2} />
+    </Document>
+  );
 }
