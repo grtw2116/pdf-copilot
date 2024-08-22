38cc093ff752af69eaf79be31ba0eaea9a466ad0
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2024-08-22 19:27:26 +0900

    feat(config): add path alias and include src in tsconfig

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,23 +1,29 @@
-import type { ConfigEnv, UserConfig } from 'vite';
-import { defineConfig } from 'vite';
-import { pluginExposeRenderer } from './vite.base.config';
+import type { ConfigEnv, UserConfig } from "vite";
+import { defineConfig } from "vite";
+import { pluginExposeRenderer } from "./vite.base.config";
 
 // https://vitejs.dev/config
 export default defineConfig((env) => {
-  const forgeEnv = env as ConfigEnv<'renderer'>;
+  const forgeEnv = env as ConfigEnv<"renderer">;
   const { root, mode, forgeConfigSelf } = forgeEnv;
-  const name = forgeConfigSelf.name ?? '';
+  const name = forgeConfigSelf.name ?? "";
 
   return {
     root,
     mode,
-    base: './',
+    base: "./",
     build: {
       outDir: `.vite/renderer/${name}`,
     },
     plugins: [pluginExposeRenderer(name)],
     resolve: {
       preserveSymlinks: true,
+      alias: [
+        {
+          find: "@",
+          replacement: "./src",
+        },
+      ],
     },
     clearScreen: false,
   } as UserConfig;
